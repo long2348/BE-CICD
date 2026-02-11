881c8deda2335fce45abab95391d0751de541ec4
tamtm0109 <tamtm@chatweb.to>
2026-02-11 11:16:18 +0700

    update v2 docker

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const app = express();
 
 app.get('/', (req, res) => {
     console.log('hello 489')
-   res.json('hello world!') 
+   res.json('hello world v2!') 
 });
 
-app.listen(8080, () => {
+app.listen(8082, () => {
     console.log('App started successfully!')
 })
